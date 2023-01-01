f89267de46304f58a26209cb45b2b64049916962
inhibitor1217 <inhibitor@kaist.ac.kr>
2023-01-01 17:13:06 +0900

    feat: enable terminal raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    error::Error,
+    io::{self, Read},
+};
+
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+
+type MainError = Box<dyn Error>;
+
+fn main() -> Result<(), MainError> {
+    enable_raw_mode()?;
+
+    for byte in io::stdin().bytes() {
+        let ch = byte.unwrap() as char;
+        println!("{}", ch);
+
+        if ch == 'q' {
+            break;
+        }
+    }
+
+    disable_raw_mode()?;
+
+    Ok(())
 }
