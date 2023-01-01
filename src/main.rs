0ec465cd9584cfa459fe1c135a5565aea9e57d4e
inhibitor1217 <inhibitor@kaist.ac.kr>
2023-01-01 17:43:13 +0900

    refactor: handle and parse key events via crossterm

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,32 @@
 use std::{
     error::Error,
-    io::{self, Read, Write},
+    io::{self, Write},
 };
 
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use crossterm::{
+    event::{read, Event, KeyCode, KeyModifiers},
+    terminal::{disable_raw_mode, enable_raw_mode},
+};
 
 type MainError = Box<dyn Error>;
 
-fn to_ctrl_byte(c: char) -> u8 {
-    (c as u8) & 0b0001_1111
-}
-
-fn die(e: io::Error) {
+fn _die(e: io::Error) {
     panic!("{}", e);
 }
 
 fn main() -> Result<(), MainError> {
     enable_raw_mode()?;
 
-    let stdin = io::stdin();
     let mut stdout = io::stdout();
 
-    for byte in stdin.bytes() {
-        match byte {
-            Ok(b) => {
-                let c = b as char;
-
-                if c.is_control() {
-                    write!(stdout, "{:?} \r\n", b)?;
-                } else {
-                    write!(stdout, "{:?} ({})\r\n", b, c)?;
-                }
-
-                if b == to_ctrl_byte('q') {
-                    break;
-                }
-            }
-            Err(e) => die(e),
+    loop {
+        match read()? {
+            Event::Key(event) => match (event.modifiers, event.code) {
+                (KeyModifiers::CONTROL, KeyCode::Char('q')) => break,
+                (_, KeyCode::Char(c)) => write!(stdout, "{:?} ({}) \r\n", c as u8, c)?,
+                (_, code) => write!(stdout, "{:?} \r\n", code)?,
+            },
+            _ => {}
         }
     }
 
