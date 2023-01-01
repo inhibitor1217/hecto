058e719743894d88a079711430bf350e1629475a
inhibitor1217 <inhibitor@kaist.ac.kr>
2023-01-01 17:29:31 +0900

    feat: handle errors from unwrap()

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,10 @@ fn to_ctrl_byte(c: char) -> u8 {
     (c as u8) & 0b0001_1111
 }
 
+fn die(e: io::Error) {
+    panic!("{}", e);
+}
+
 fn main() -> Result<(), MainError> {
     enable_raw_mode()?;
 
@@ -18,17 +22,21 @@ fn main() -> Result<(), MainError> {
     let mut stdout = io::stdout();
 
     for byte in stdin.bytes() {
-        let b = byte.unwrap();
-        let c = b as char;
+        match byte {
+            Ok(b) => {
+                let c = b as char;
 
-        if c.is_control() {
-            write!(stdout, "{:?} \r\n", b)?;
-        } else {
-            write!(stdout, "{:?} ({})\r\n", b, c)?;
-        }
+                if c.is_control() {
+                    write!(stdout, "{:?} \r\n", b)?;
+                } else {
+                    write!(stdout, "{:?} ({})\r\n", b, c)?;
+                }
 
-        if b == to_ctrl_byte('q') {
-            break;
+                if b == to_ctrl_byte('q') {
+                    break;
+                }
+            }
+            Err(e) => die(e),
         }
     }
 
