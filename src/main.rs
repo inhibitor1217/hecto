4c005a2d6611375f2103dd9241ac75478ba25e5a
inhibitor1217 <inhibitor@kaist.ac.kr>
2023-01-01 17:26:29 +0900

    feat: use ctrl+q to quit

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,10 @@ use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 
 type MainError = Box<dyn Error>;
 
+fn to_ctrl_byte(c: char) -> u8 {
+    (c as u8) & 0b0001_1111
+}
+
 fn main() -> Result<(), MainError> {
     enable_raw_mode()?;
 
@@ -14,10 +18,16 @@ fn main() -> Result<(), MainError> {
     let mut stdout = io::stdout();
 
     for byte in stdin.bytes() {
-        let ch = byte.unwrap() as char;
-        write!(stdout, "{}", ch)?;
+        let b = byte.unwrap();
+        let c = b as char;
 
-        if ch == 'q' {
+        if c.is_control() {
+            write!(stdout, "{:?} \r\n", b)?;
+        } else {
+            write!(stdout, "{:?} ({})\r\n", b, c)?;
+        }
+
+        if b == to_ctrl_byte('q') {
             break;
         }
     }
