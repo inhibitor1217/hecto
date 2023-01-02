f5350fe7dc8089292bcd0592be9d72e9dc5b6fbc
dugi <dugi@channel.io>
2023-01-02 09:24:10 +0900

    fix: handle unicode character lengths

--- src/row.rs
+++ src/row.rs
@@ -1,24 +1,32 @@
 use std::cmp::{max, min};
 
+use unicode_segmentation::UnicodeSegmentation;
+
 pub struct Row {
     string: String,
+    len: usize,
 }
 
 impl From<&str> for Row {
     fn from(str: &str) -> Self {
         Self {
             string: String::from(str),
+            len: str.graphemes(true).count(),
         }
     }
 }
 
 impl Row {
     pub fn render(&self, start: usize, end: usize) -> String {
-        let end = max(start, min(end, self.string.len()));
-        self.string.get(start..end).unwrap_or_default().to_string()
+        let end = max(start, min(end, self.len()));
+        self.string
+            .graphemes(true)
+            .skip(start)
+            .take(end - start)
+            .collect()
     }
 
     pub fn len(&self) -> usize {
-        self.string.len()
+        self.len
     }
 }
